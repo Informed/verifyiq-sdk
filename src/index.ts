f787449ec522b67a570c64a609ac2a5f314a4117
shahen94 <shahen.hovhannisyan.94@gmail.com>
2020-07-23 14:34:48 +0400

    fix(TS): Corrected typedefs

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import VerifyIQ from './sdk';
 import { AuthTypes } from './types/auth-types.enum';
 import { EventsEnum } from './types/events.enum';
-import { IPCSerializable, IpcMessage } from './types/ipc.interface';
+import { IpcMessage } from './types/ipc.interface';
 
+// ES6 module exported for type defs
 export default VerifyIQ;
 export {
   AuthTypes,
   EventsEnum,
-  IPCSerializable,
   IpcMessage
 };
 
 module.exports = VerifyIQ;
+
+module.exports.IpcMessage = IpcMessage;
+module.exports.AuthTypes = AuthTypes;
+module.exports.EventsEnum = EventsEnum;
+
+// @deprecated
 module.exports.auth = AuthTypes;
 module.exports.events = EventsEnum;
